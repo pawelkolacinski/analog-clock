c4808a1929f585bc211d664baf5320fb000a388f
Pawel Kolacinski <pawel.kolacinski@gmail.com>
2022-08-25 13:21:41 +0200

    reformatting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-
 import { useEffect, useState } from 'react';
 import './App.css';
 import Clock from './components/Clock';
 
 function App() {
   const getCurrentTime = () => {
-    const time = new Date()
+    const time = new Date();
     return {
       hour: time.getHours(),
       minute: time.getMinutes(),
-      second: time.getSeconds()
-    }
-  } 
+      second: time.getSeconds(),
+    };
+  };
 
-  const [time, setTime] = useState(getCurrentTime())
-  useEffect( ()=> {
-    setInterval(() => setTime(getCurrentTime()),100)
-  },[])
+  const [time, setTime] = useState(getCurrentTime());
+  useEffect(() => {
+    setInterval(() => setTime(getCurrentTime()), 100);
+  }, []);
 
   return (
     <div className="App">
